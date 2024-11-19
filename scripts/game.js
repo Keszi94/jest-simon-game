146c9f551a5c2a9fe6ba010b2e17a3eb4c8026c8
Eszter Kiskarpati <keszi94@gmail.com>
2024-11-19 16:49:13 +0000

    Add test for and create the addTurn function

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,18 @@
+/** ---JAVASCRIPT STRUCTURE---
+ * 
+ *      game { object }
+ *      newGame()
+ *      addTurn() : 
+ *          # clear the player moves array
+ *          # Randomly add a button ID to the currentGame array
+ *          # Call showTurns() function
+ *      showTurns()
+ *      lightsOn()
+ *      playerTurn()
+ *      showScore()
+ */
+
+
 const { choices } = require("yargs");
 
 let game = {
@@ -12,13 +27,20 @@ function newGame() {
     game.currentGame = [];
     game.playerMoves = [];
     showScore();
+    addTurn();
 };
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
 };
 
+function addTurn() {
+    game.playerMoves = []; // clear playerMoves array
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]); // Randomly add a button ID to the currentGame array
+    // showTurns(); // Call showTurns() function
+}
 
 
 
-module.exports = { game, newGame, showScore };
+
+module.exports = { game, newGame, showScore, addTurn };
